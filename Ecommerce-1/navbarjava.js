b841232ac862c6c7d37aae903230195309488489
Bvaishnavi123 <97448974+Bvaishnavi123@users.noreply.github.com>
2022-07-13 13:04:00 +0530

    Initial commit

---
+++ Ecommerce-1/navbarjava.js
@@ -0,0 +1,17 @@
+ // Make EventListner For Each And Every div
+ document.querySelector("#signup").addEventListener("click", function () {
+    window.location.href = "Signup.html";
+  });
+
+  document.querySelector("#login").addEventListener("click", function () {
+    window.location.href = "Login.html";
+  });
+  document.querySelector("#men").addEventListener("click", function () {
+    window.location.href = "men.html";
+  });
+  document.querySelector("#women").addEventListener("click", function () {
+    window.location.href = "women.html";
+  });
+  document.querySelector("#product").addEventListener("click", function () {
+    window.location.href = "product.html";
+  });
